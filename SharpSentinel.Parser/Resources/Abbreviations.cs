a1630f1a5162cc2699024c2ae89e58ea29aaaad1
Yannik Hackbarth <haggiderzweite@googlemail.com>
2018-06-30 11:21:19 +0200

    Add abbreviations - lots more to come

--- SharpSentinel.Parser/Resources/Abbreviations.cs
+++ SharpSentinel.Parser/Resources/Abbreviations.cs
@@ -19,14 +19,64 @@ namespace SharpSentinel.Parser.Resources
             return abbreviation.GetDescription();
         }
 
+        /// <summary>
+        /// Gerneral sentinel related abbreviations
+        /// </summary>
         public enum Abbreviation
         {
-            [Description("Information Processing Facility")]
+            [Description("Annotation Data Set")]
+            ADS,
+            [Description("ADSR")]
+            ADSR,
+            [Description("Instrument Processing Facility")]
             IPF,
             [Description("Single Look Complex")]
-            SLC ,
+            SLC,
+            [Description("Geo-reference Tag Image File Format")]
+            GeoTIFF,
+            [Description("Global Monitoring for Environment and Security")]
+            GMES,
+            [Description("Geophysical Model Function")]
+            GMF,
+            [Description("GRD")]
+            GRD,
+            [Description("Horizontal polarisation (Tx & Rx)")]
+            HH,
+            [Description("High Resolution")]
+            HR,
+            [Description("Medium Resolution")]
+            MR,
+            [Description("Horizontal Vertical polarisation")]
+            HV,
+            [Description("In-phase/Quadrature")]
+            IQ,
+            [Description("Interface Control Document")]
+            ICD,
+            [Description("Integrated Slide Lobe Ratio")]
+            ISLR,
+            [Description("Instrument Source Packet")]
+            ISP,
+            [Description("Interferometric Wide Swath")]
+            IW,
+            [Description("Look-up Table")]
+            LUT,
+            [Description("Measurement Data Set")]
+            MDS,
+            [Description("Normalised Radar Cross Section")]
+            NRCS,
+            [Description("L2 Ocean Product")]
+            OCN,
+            [Description("Ocean Swell Spectra")]
+            OSW,
+            [Description("Ocean Wind Field")]
+            OWI,
+            [Description("Preliminary Design Review")]
+            PDR
         }
-
+        
+        /// <summary>
+        /// XML related abbreviations
+        /// </summary>
         public enum XMLAbbreviation
         {
             [Description("Description Information")]
