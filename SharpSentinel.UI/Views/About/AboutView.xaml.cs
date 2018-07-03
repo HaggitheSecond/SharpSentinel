627eaa3708f88e541843cf52964fdbf51209b54a
Yannik Hackbarth <haggiderzweite@googlemail.com>
2018-07-03 23:07:27 +0200

    Add about page

--- SharpSentinel.UI/Views/About/AboutView.xaml.cs
+++ SharpSentinel.UI/Views/About/AboutView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,6 +30,12 @@ namespace SharpSentinel.UI.Views.About
         public WindowSettings GetWindowSettings()
         {
             return WindowSettings.Standard();
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
